dd05830b74ee6d059f1d54f0e267f0847b5142ce
Josue Amorim <josuedesouzaamorim@gmail.com>
2021-11-10 11:22:30 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Josué Amorim on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
