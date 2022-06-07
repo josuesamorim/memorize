a513d3bb78b319162f7ad3719bc1f3e150972a0b
Josue Amorim <josuedesouzaamorim@gmail.com>
2022-06-07 10:04:47 -0300

    I created the struct of cardView and added them to HStack inside of contentView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,15 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View{
+    var body: some View{
+        HStack {
+            CartView()
+            CartView()
+            CartView()
+            CartView()
+        }
+       .padding(.horizontal)
+       .foregroundColor(.red)
     }
 }
+
+struct CartView: View {
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack(content: {
+            
+            let shape = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font((.largeTitle))
+            } else {
+                shape.fill()
+            }
+        })
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
